835d9b1c2fe1293ecac4c2a15c0ded302bfb22b8
Flix0Flix <felix.karlstrom07@gmail.com>
2026-02-17 13:16:12 +0200

    Add BetterAuth route

---
+++ lib/auth.ts
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "@better-auth/prisma-adapter";
+import { nextCookies } from "better-auth/next-js";
+import prisma from "@/lib/prisma";
+
+export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+  database: prismaAdapter(prisma, { provider: "postgresql" }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+  plugins: [nextCookies()],
+});
